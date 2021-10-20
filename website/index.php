b0c19c559034bb742289f5803070d3f7236a737e
James Heaney <40678238+Jvheaney@users.noreply.github.com>
2021-10-19 21:26:25 -0400

    Update index.php

--- website/index.php
+++ website/index.php
@@ -4,7 +4,7 @@ include '../local_utilities.php'; //This file contains helpful functions and mod
 //Now we will call our session creation function.
 $SESSION_HASH = createSession();
 
-//We inject a script tag to the start of the page to insert out session hash.
+//We inject a script tag to the start of the page to insert our session hash.
 echo "<script>window.token='$SESSION_HASH';</script>";
 
 //And now we will render our html.
